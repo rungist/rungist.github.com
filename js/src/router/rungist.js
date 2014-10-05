55dd70a8bc1df0dc3bd1fe84fe6a653eb9d556c9
Jichao Ouyang <oyanglulu@gmail.com>
2014-10-05 21:39:27 +0800

    pass gistid and username as property for RunGist component

--- js/src/router/rungist.js
+++ js/src/router/rungist.js
@@ -3,7 +3,7 @@ var React = require('react');
 var RunGistPage = require('../components/rungist.react');
 module.exports = function(){
   React.renderComponent(
-      <RunGistPage/>,
-      document.getElementById('main-container')
+    <RunGistPage username={this.username} gistid={this.gistid}/>,
+    document.getElementById('main-container')
   );
 };
