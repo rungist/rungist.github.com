30baead6343e61838c5f5d90b83a9dfaca9ba76d
Jichao Ouyang <oyanglulu@gmail.com>
2014-10-09 23:25:03 +0800

    add zero clipboard

--- js/src/components/rungist.react.js
+++ js/src/components/rungist.react.js
@@ -34,14 +34,22 @@ var RunGistPage = React.createClass({
   componentDidUpdate: function(){
     var codeblock = document.querySelector('.result .codeblock');
     aceHighlighter(codeblock, 'javascript');
+    var clip = new ZeroClipboard($("#result-clipboard"));
+    clip.on("ready", function() {
+      console.log("Flash movie loaded and ready.");
+      this.on("aftercopy", function(event) {
+        $("#result-clipboard").popup({on:"click"}).popup("show")
+      });
+    }); 
   },
   render: function(){
     return (
       <div className="ui grid">
         <div className="ten wide column">
           <MetaData gistid={this.props.gistid} username={this.props.username}/>
-          <div className="ui segment result">
-            <div className="codeblock" id="editor">
+        <div className="ui segment result">
+          <button class="ui top right attached label" data-content="copied" data-variation="inverted" id="result-clipboard" data-clipboard-target="result-codeblock">Copy</button>
+            <div className="codeblock" id="result-codeblock">
               {JSON.stringify(this.state.result, undefined, 2)}
             </div>
           </div>
